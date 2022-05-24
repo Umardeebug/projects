94a4a5c4e71a07ef805d7c0742cd02869b905a31
Umardeebug <favormomoh0.gmail.com>
2022-05-24 11:40:47 +0100

    kablamo

---
+++ week6/variable.js
@@ -0,0 +1,16 @@
+let name = "umar_momoh",
+  age = 20,
+  salary = 1010;
+
+let sum = (100 + 50) * age;
+
+let x = 6;
+let y = 3;
+
+console.log(name);
+console.log(age);
+console.log(salary);
+console.log(sum);
+console.log(x < 10 && y > 1);
+console.log(typeof age);
+console.log([typeof x, y, age]);
