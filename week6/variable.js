c46ff18ac93cd88e0cb2fb0521e85628546579cb
Umardeebug <favormomoh0.gmail.com>
2022-05-28 19:55:46 +0100

    making progress

--- week6/variable.js
+++ week6/variable.js
@@ -1,16 +1,52 @@
-let name = "umar_momoh",
-  age = 20,
-  salary = 1010;
+// let name = "umar_momoh",
+//   age = 20,
+//   salary = 1010;
 
-let sum = (100 + 50) * age;
+// let sum = (100 + 50) * age;
 
-let x = 6;
-let y = 3;
+// let x = 6;
+// let y = 3;
 
-console.log(name);
-console.log(age);
-console.log(salary);
-console.log(sum);
-console.log(x < 10 && y > 1);
-console.log(typeof age);
-console.log([typeof x, y, age]);
+// console.log(name);
+// console.log(age);
+// console.log(salary);
+// console.log(sum);
+// console.log(x < 10 && y > 1);
+// console.log(typeof age);
+// console.log([typeof x, y, age]);
+
+// let hour = 22;
+// let greeting;
+// if (hour < 10) {
+//   greeting = "good day";
+// } else if (hour > 10 && hour < 20) {
+//   greeting = "good evening";
+// } else {
+//   greeting = "good night";
+// }
+
+// console.log(greeting);
+
+let age = "john";
+age = Number(age);
+if (isNaN(age)) {
+  voteable = "input is not a number";
+} else {
+  voteable = age < 18 ? "too young" : "old enough";
+}
+console.log(voteable);
+
+// let str = "apple, Banana, Kiwi";
+// let part = str.slice(7, 13);
+// let part2 = str.slice(0, 5);
+// console.log(part);
+// console.log(part2);
+
+// let text = "please visit medplan and medplan!";
+// let newtext = text.replace(/medplan/g, "deebug");
+// console.log(newtext);
+
+// let text1 = "hello";
+// let text2 = "world";
+// let text3 = text1.concat("  ", text2);
+// console.log(text3);
