dba6b36cd076c3f4d52645251f18dc4f100f8e4c
Umardeebug <favormomoh0.gmail.com>
2022-06-03 22:08:21 +0100

    problem 1 done

--- assignments/javascripttasks/debug.js
+++ assignments/javascripttasks/debug.js
@@ -1,4 +1,4 @@
-// PROBELM SET 1 STARTS HERE......
+// PROBELM SET 1 STARTS HERE........................................................................................
 
 // NUMBER 1 QUESTION
 
@@ -47,24 +47,165 @@
 // }
 
 // NUMBER 4 QUESTION HERE              <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-let fname = umar;
-let sname = momoh;
-let pc = hp;
-let phone = redmi;
-let hair = black;
-let color = red;
-let shoesize = "45";
-let table = yellow;
-let x = Infinity;
-let y = 0;
-let emergency = 911;
-let car = urus;
-let middlename = favor;
-let height = 6.1;
-let game = fifa;
-let water = notredame;
-let character = ezio;
-let magic = merlin;
-let nepa = nolight;
-let gen = light;
-let gun = deagle;
+// let fname = umar;
+// let sname = momoh;
+// let pc = hp;
+// let phone = redmi;
+// let hair = black;
+// let color = red;
+// let shoesize = "45";
+// let table = yellow;
+// let x = Infinity;
+// let y = 0;
+// let emergency = 911;
+// let car = urus;
+// let middlename = favor;
+// let height = 6.1;
+// let game = fifa;
+// let water = notredame;
+// let character = ezio;
+// let magic = merlin;
+// let nepa = nolight;
+// let gen = light;
+// let gun = deagle;
+
+// NUMBER 5 QUESTION HERE              <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// const cars = ["urus", "toyota", "jeep", "golf", "camry", "venza"];
+
+// const ages = [22, 44, 55, 66, 77, 88, 99];
+
+// const phones = ["redmi", "iphone", "samsung", "tecno", "itel", "rog"];
+
+// const maleNames = ["john", "jerry", "max", "umar", "dan", "stephen"];
+
+// const femaleNames = [
+//   "mary",
+//   "fatima",
+//   "alice",
+//   "dorothy",
+//   "katherine",
+//   "chioma",
+// ];
+// const clotingBrands = [
+//   "dior",
+//   "gucci",
+//   "channel",
+//   "fendi",
+//   "birkin",
+//   "vuitton",
+// ];
+// const appliances = ["frige", "iron", "tv", "fan", "vacuum", "oven"];
+
+// const professions = [
+//   "engineer",
+//   "accountant",
+//   "scientist",
+//   "economist",
+//   "artist",
+// ];
+// professions.push("musician");
+
+// let animals = ["rat", "bat", "cat", "donkey", "dog"];
+// animals.push("turkey");
+
+// const scores = [82, 95, 88, 92, 72];
+// scores.push(99);
+
+// console.log(animals);
+// console.log(scores);
+// console.log(professions);
+// console.log(cars);
+// console.log(maleNames);
+
+// // NUMBER 6 QUESTION HERE              <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// const me = {
+//   firstName: "umar",
+//   lastName: "momoh",
+//   age: 100,
+//   color: "red",
+//   height: 6.1,
+// };
+
+// const mum = {
+//   fname: "dorothy",
+//   lname: "jega",
+//   age: 200,
+//   color: "blue",
+//   height: 5.6,
+// };
+
+// let animalobjects = {
+//   reptile: "snake",
+//   mammal: "human",
+//   fish: "titus",
+//   pet: "dog",
+//   predator: "lion",
+// };
+
+// let games = {
+//   ps4: "wildhunt",
+//   ps3: "gtaiv",
+//   pc: "csgo",
+//   xbox: "halo",
+//   mobile: "pubg",
+// };
+
+// let schools = {
+//   uniben: "benin",
+//   unilag: "lagos",
+//   unimed: "ondo",
+//   biu: "benin",
+//   aau: "ekpoma",
+// };
+
+// let genre = {
+//   horror: "theNun",
+//   comedy: "bruceAlmighty",
+//   action: "missionimpossible",
+//   scifi: "interstellar",
+//   thriller: "dontBreathe",
+// };
+
+// let subjects = {
+//   chemistry: "atoms",
+//   biology: "mitosis",
+//   physics: "gravity",
+//   agriculture: "soil",
+//   economics: "demand",
+//   math: "fractions",
+// };
+
+// let socials = {
+//   face: "book",
+//   whatsapp: "zuck",
+//   instagram: "fakelife",
+//   snapchat: "xsite",
+//   twitter: "xsiteii",
+// };
+
+// let anime = {
+//   naruto: "sakura",
+//   deadly: "escanor",
+//   aot: "eren",
+//   black: "asta",
+//   devil: "dante",
+// };
+
+// let food = {
+//   cereal: "rice",
+//   fruit: "banana",
+//   vegtable: "okro",
+//   carbohydrate: "yam",
+//   protein: "milk",
+// };
+
+// console.log(me);
+// console.log(genre);
+// console.log(socials);
+// console.log(food);
+// console.log(games);
+
+// // NUMBER 7 QUESTION ABOVE             <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// PROBELM SET 2 STARTS HERE........................................................................................
