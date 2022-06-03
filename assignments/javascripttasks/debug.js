e953e6e67a807312f75e9c90874ead2a2786620f
Umardeebug <favormomoh0.gmail.com>
2022-06-03 14:28:46 +0100

    omoo

--- assignments/javascripttasks/debug.js
+++ assignments/javascripttasks/debug.js
@@ -2,19 +2,19 @@
 
 // NUMBER 1 QUESTION
 
-let time = 12;
-let greeting;
-if (time < 6) {
-  greeting = "early in the morning";
-} else if (time < 12) {
-  greeting = "good morning";
-} else if (time < 16) {
-  greeting = "good afternoon";
-} else if (time < 20) {
-  greeting = "good evening";
-} else greeting = "good night";
+// let time = 12;
+// let greeting;
+// if (time < 6) {
+//   greeting = "early in the morning";
+// } else if (time < 12) {
+//   greeting = "good morning";
+// } else if (time < 16) {
+//   greeting = "good afternoon";
+// } else if (time < 20) {
+//   greeting = "good evening";
+// } else greeting = "good night";
 
-console.log(greeting);
+// console.log(greeting);
 
 // NUMBER 2 QUESTION
 
@@ -22,14 +22,49 @@ console.log(greeting);
 //   console.log(i);
 // }
 
-for (let i = 0; i <= 11; i++) {
-  if (i === 4) {
-    continue;
-  }
-  if (i === 5) {
-    continue;
-  }
-  console.log(i);
-}
+// for (let i = 0; i <= 11; i++) {
+//   if (i === 4) {
+//     continue;
+//   }
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
 
-// Number 3 QUESTION
+// Number 3 QUESTION       <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// for (let i = 10; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 20; i <= 30; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// NUMBER 4 QUESTION HERE              <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+let fname = umar;
+let sname = momoh;
+let pc = hp;
+let phone = redmi;
+let hair = black;
+let color = red;
+let shoesize = "45";
+let table = yellow;
+let x = Infinity;
+let y = 0;
+let emergency = 911;
+let car = urus;
+let middlename = favor;
+let height = 6.1;
+let game = fifa;
+let water = notredame;
+let character = ezio;
+let magic = merlin;
+let nepa = nolight;
+let gen = light;
+let gun = deagle;
